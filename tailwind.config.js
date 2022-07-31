7a016870ea3d282b5ca1f9cba38c200746318758
unknown <komandakov96@gmail.com>
2022-07-31 19:57:19 +0300

    init porject

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'blue-bg':'#F5F8FF',
+        'text-blue':'#3D4F9F',
+        'buttom-blur':'#4C85FF',
+      }
+    },
+  },
+  plugins: [],
+}
